134667be354d349494721b9e2067d5cb652553f4
Elizabeth <elizabethnich19@gmail.com>
2017-04-05 22:04:14 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,12 @@ class App < Sinatra::Base
     erb :'/super_hero'
     end
 
-
     post '/teams' do
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       members = params[:team][:members]
-      @member = members.collect do |member_params|
-        Member.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
+      @member = members.collect do |params|
+        Member.new({name: params[:name], power: params[:power], bio: params[:bio]})
       end
-
-
       erb :'/team'
     end
 end
